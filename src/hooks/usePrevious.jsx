f2a386769f558f35b65b042b76f36f959e8d8da7
Estevam Morais <33493699+estevamorais@users.noreply.github.com>
2023-01-02 16:44:06 -0300

    UseDebugValue

--- src/hooks/usePrevious.jsx
+++ src/hooks/usePrevious.jsx
@@ -1,7 +1,10 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useDebugValue } from "react";
 
 export const usePrevious = (value) => {
   const ref = useRef();
+
+  useDebugValue("--- CUSTOM HOOK ADD USEDEBUGVALUE---");
+  useDebugValue(`Previous value is: ${ref.current}`);
 
   useEffect(() => {
     ref.current = value;
